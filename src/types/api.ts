160e232d7a3731222fba2cbb774e6e3c3bb6ab0a
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-05-17 14:44:00 +0000

    initial: create stub and add typescript dep, and two data types

---
+++ src/types/api.ts
@@ -0,0 +1,33 @@
+export interface Testimonial {
+  name: string;
+  pic: string; // base64 encoded thumb most probably
+  translated_text: string;
+  activities: string[];
+}
+
+export enum EventN4DType {
+  PARTY = "party",
+  WORKSHOP = "workshop",
+}
+
+export interface EventN4D {
+  title: string;
+  subTitle?: string;
+  hostName?: string;
+  date: Date;
+  dateEnd?: Date;
+  type: EventN4DType;
+  pic?: string; // or standard one depending on type
+  time: string;
+  location: string; // address
+  locationLink?: string; // to google maps
+  locationComment?: string; // how to spot
+  description: string;
+  shortDescription: string; // for card view
+  linkRSVP: string; // registration form
+  followUpText?: string;
+  followUpLink?: string; // the adjacent event
+  additionalTitle?: string;
+  additionalInfo?: string[]; // lineup, content, etc.
+  outro?: string;
+}
