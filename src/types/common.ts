00754d8fada4a25403ff26f9f05472c7705acfe2
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-05-20 17:10:47 +0000

    add: common types

---
+++ src/types/common.ts
@@ -0,0 +1,21 @@
+export enum Lang {
+  EN = "en",
+  DE = "de",
+  RU = "ru",
+  AR = "ar",
+  FA = "fa",
+  TR = "tr",
+  ES = "es",
+}
+
+export enum HttpMethod {
+  GET = "GET",
+  HEAD = "HEAD",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+  TRACE = "TRACE",
+  OPTIONS = "OPTIONS",
+  CONNECT = "CONNECT",
+  PATCH = "PATCH",
+}
