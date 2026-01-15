f14f199cf406bbc067959576680e3273df8e4899
arturasmckwcz <arturasmckwcz@gmail.com>
2026-01-15 11:41:24 +0000

    add: patch

--- src/types/api/communication.ts
+++ src/types/api/communication.ts
@@ -36,3 +36,4 @@ export interface ApiCommunicationGet extends ApiVolunteerCommunicationPost {
   userId: number;
 }
 
+export interface ApiVolunteerCommunicationPatch extends ApiVolunteerCommunicationPost {}; 
