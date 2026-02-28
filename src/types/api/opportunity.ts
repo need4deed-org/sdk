ea388e97eb3aea4a540d832777c1e681fae9dbf5
arturasmckwcz <arturasmckwcz@gmail.com>
2026-02-28 17:35:24 +0000

    add: legacy form data type

--- src/types/api/opportunity.ts
+++ src/types/api/opportunity.ts
@@ -70,6 +70,34 @@ export interface OpportunityFormData {
   last_edited_time_notion?: string;
 }
 
+export interface OpportunityLegacyFormData {
+  title: string;
+  rac_email: string;
+  rac_full_name: string;
+  rac_phone: string;
+  rac_address: string;
+  rac_plz: string;
+  opportunity_type: "accompanying" | "volunteering";
+  accomp_address: string;
+  accomp_postcode: string;
+  accomp_datetime?: string;
+  accomp_name?: string;
+  accomp_phone?: string;
+  accomp_information?: string;
+  accomp_translation?: `${TranslatedIntoType}`;
+  berlin_locations?: OptionId[];
+  languages: OptionId[];
+  activities: OptionId[];
+  skills: OptionId[];
+  timeslots?: [number, OptionId][];
+  volunteers_number: number;
+  vo_information?: string;
+  category: string;
+  category_id: OptionId;
+  last_edited_time_notion?: string;
+  language: `${Lang}`;
+}
+
 export interface ApiOpportunityAgent {
   type: AgentType;
   name: string;
