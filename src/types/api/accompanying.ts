1529ce608c3e5452f72551b8442c6a0eb3d8f285
arturasmckwcz <arturasmckwcz@gmail.com>
2026-03-11 11:32:20 +0000

    update: date types

--- src/types/api/accompanying.ts
+++ src/types/api/accompanying.ts
@@ -2,8 +2,8 @@ import { TranslatedIntoType } from "./opportunity";
 
 export interface ApiOpportunityAccompanyingDetails {
   appointmentAddress?: string;
-  appointmentDate?: Date;
-  appointmentTime?: Date;
+  appointmentDate?: string;
+  appointmentTime?: string;
   refugeeNumber?: string;
   refugeeName?: string;
   languageToTranslate?: TranslatedIntoType;
