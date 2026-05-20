575730a7b6c8ea420112c230d2c5889c645014fa
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2026-05-20 13:17:36 +0200

    add: generics for voidable fields

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,1 +1,7 @@
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export type VoidableProps<T> = {
+  [K in keyof T]?: T[K] | undefined | null;
+};
+
+export type Voidable<T> = T | undefined | null;
