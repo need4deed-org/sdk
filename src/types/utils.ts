bb3d93aab6bed89d0b91898b72194a12f505fd46
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2026-05-22 13:32:28 +0200

    update: voidable props generic

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,7 +1,11 @@
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
-export type VoidableProps<T> = {
-  [K in keyof T]?: T[K] | undefined | null;
+export type VoidableProps<T, K extends keyof T = keyof T> = Omit<T, K> & {
+  [P in K]?: T[P] | null;
+};
+
+export type VoidableUndefined<T> = {
+  [K in keyof T]: undefined extends T[K] ? T[K] | null : T[K];
 };
 
 export type Voidable<T> = T | undefined | null;
